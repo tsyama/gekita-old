3ea75c4be3ac4fd831391035717b1c7d2362e551
tsyama <tsyama.desu@gmail.com>
2018-12-18 00:42:27 +0900

    [Add] Basic preview function

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -23,6 +23,49 @@ var gekiBody = new Vue({
         body: '',
     },
     updated: function() {
-        gekiPreview.body = this.body;
+        gekiPreview.body = gekiParser(this.body);
+    },
+    methods: {
+        tabber: function(e) {
+            e.preventDefault();
+            var elem = e.target;
+            var val = elem.value;
+            var pos = elem.selectionStart;
+            elem.value = val.substr(0, pos) + '\t' + val.substr(pos, val.length);
+            elem.setSelectionRange(pos + 1, pos + 1);
+        }
     }
 });
+
+function gekiParser(text)
+{
+    var lines = text.split("\n");
+
+    var result = '';
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        result += lineParser(line);
+    }
+    return result;
+}
+
+function lineParser(line)
+{
+    if (line === '') {
+        return '<tr><td colspan="2">　</td></tr>';
+    }
+    var titleCheck = line.match(/^# (.*)/);
+    if (titleCheck) {
+        return '<tr><td colspan="2"><p class="title">' + titleCheck[1] + '</p></td></tr>';
+    }
+
+    var dooCheck = line.match(/^\t(.*)/);
+    if (dooCheck) {
+        return '<tr><td colspan="2"><span>　' + dooCheck[1] + '</span></td></tr>'
+    }
+    var serifs = line.split("\t");
+    if (serifs.length >= 2) {
+        line = '<tr><td><span class="tag">' + serifs[0] + '</span></td><td><span>' + serifs[1] + '</span></td></tr>';
+    }
+    return line;
+}
